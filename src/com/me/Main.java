f0393bcb90283f2a048bbabd221f9f390c4bae9e
patrickatcw <pj8392@yahoo.com>
2018-03-28 11:04:20 -0400

    code input for searchtree class, ran in main after making some changes for new code, obtained results

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -9,16 +9,41 @@ public class Main {
     public static void main(String[] args) {
 
         //step 21 instance
-        MyLinkedList list = new MyLinkedList(null);
+        //MyLinkedList list = new MyLinkedList(null);
+
+        //step 38 change the above
+        SearchTree tree = new SearchTree(null);
+        //step 39 change all list to tree, then comment out
+
+        //step 40, once those are changed, go back to searchtree
+        //and add our additem methods
+
+        //step 41 back from searchtree to run
+        /*
+        results;(sorted in order)
+        0
+        1
+        2
+        3
+        4
+        5
+        6
+        7
+        8
+        9*/
 
         //step 22 call method
-        list.traverse(list.getRoot());
+        tree.traverse(tree.getRoot());
+
+        //step 23 run
+        //result; The list is empty
+
 
         //given code
         //create a string data array to avoid typing loads of additems instructions
-        //String stringData = "Columbus Akron Dayton Canton Cleveland Cadiz Lorain Cleveland";
+        //String stringData = "Darwin Brisbane Perth Melbourne Canberra Adelaide Sydney Canberra";
 
-        //step 28 change the above to.......
+        //step 28 change the above to....
         String stringData = "5 7 3 9 8 2 1 0 4 6";
         //step 29 run
         /*
@@ -35,62 +60,61 @@ public class Main {
         8
         9*/
 
-        //step 23 run
-        //result; The list is empty
-
         String[]data = stringData.split(" ");
         for(String s : data){
 
             //step 24 calling method
-            list.addItem(new Node(s));
+            tree.addItem(new Node(s));
 
         }
 
         //step 25 calling method
-        list.traverse(list.getRoot());
+        tree.traverse(tree.getRoot());
 
         //step 26 run
-        /*
-        results;
-        The list is empty
-        Cleveland is already present, not added.
-        Akron
-        Cadiz
-        Canton
-        Cleveland
-        Columbus
-        Dayton
-        Lorain*/
+            /*
+            results;
+            The list is empty
+            Canberra is already present, not added.
+            Adelaide
+            Brisbane
+            Canberra
+            Darwin
+            Melbourne
+            Perth
+            Sydney
+            */
 
         //step 27 return to MyLinkedList public boolean removeItem
 
         //step 30 calling on those methods
-        list.removeItem(new Node("3"));
-        list.traverse(list.getRoot());
+        /*tree.removeItem(new Node("3"));
+        tree.traverse(tree.getRoot());
 
-        list.removeItem(new Node("5"));
-        list.traverse(list.getRoot());
+        tree.removeItem(new Node("5"));
+        tree.traverse(tree.getRoot());
 
-        list.removeItem(new Node("0"));
-        list.removeItem(new Node("4"));
-        list.removeItem(new Node("2"));
-        list.traverse(list.getRoot());
+        tree.removeItem(new Node("0"));
+        tree.removeItem(new Node("4"));
+        tree.removeItem(new Node("2"));
+        tree.traverse(tree.getRoot());
 
-        list.removeItem(new Node("9"));
-        list.traverse(list.getRoot());
-        list.removeItem(new Node("8"));
-        list.traverse(list.getRoot());
-        list.removeItem(new Node("6"));
-        list.traverse(list.getRoot());
-        list.removeItem(list.getRoot());
-        list.traverse(list.getRoot());
-        list.removeItem(list.getRoot());
-        list.traverse(list.getRoot());
+        tree.removeItem(new Node("9"));
+        tree.traverse(tree.getRoot());
+        tree.removeItem(new Node("8"));
+        tree.traverse(tree.getRoot());
+        tree.removeItem(new Node("6"));
+        tree.traverse(tree.getRoot());
+        tree.removeItem(tree.getRoot());
+        tree.traverse(tree.getRoot());
+        tree.removeItem(tree.getRoot());
+        tree.traverse(tree.getRoot());*/
 
         //step 31 run
         //obtained results
 
+        //step 32 working on SEARCHTREE
+        //step 32 create searchTree class
+
     }
-
 }
-
--- src/com/me/SearchTree.java
+++ src/com/me/SearchTree.java
@@ -1,4 +1,87 @@
 package com.me;
 
-public class SearchTree {
+//step 32 create search tree class
+//step 33 implements NodeList
+public class SearchTree implements NodeList{
+
+    //step 34 create field
+    private ListItem root = null;
+
+    //step 35 constructor
+    public SearchTree(ListItem root) {
+        this.root = root;
+    }
+
+    //step 36 implement methods override
+    @Override
+    public ListItem getRoot() {
+        return this.root;
+    }
+
+    @Override
+    public boolean addItem(ListItem newItem) {
+        //step 40 addItem method(keeps traversing the tree)
+        if (this.root==null) {
+            //the tree was empty, so our item becomes the head of the tree
+            this.root = newItem;
+            return true;
+        }
+
+        //otherwise, start comparing from the head of the tree
+        ListItem currentItem = this.root;
+        while (currentItem!=null) {
+            int comparison = (currentItem.compareTo(newItem));
+            if (comparison < 0) {
+                //newItem is greater, move right if possible
+                if (currentItem.next()!=null) {
+                    currentItem = currentItem.next();
+                } else {
+                    //there is no node to the right, so add at this point
+                    currentItem.setNext(newItem);
+                    return true;
+                }
+            } else if (comparison > 0) {
+                //newItem is less, move left if possible
+                if (currentItem.previous()!=null) {
+                    currentItem = currentItem.previous();
+                } else {
+                    //there is no node to the left, so add at this point
+                    currentItem.setPrevious(newItem);
+                    return true;
+                }
+            }else {
+                //equal, so don't add
+                System.out.println(newItem.getValue() + " is already present");
+                return false;
+            }
+        }
+        //we can't actually get here, but Java complains if there is no return
+        return false;
+
+        //step 41 go back to main to run
+
+    }
+
+
+    @Override
+    public boolean removeItem(ListItem item) {
+        return false;
+    }
+
+    @Override
+    public void traverse(ListItem root) {
+        //step 37 ***RECURSIVE METHOD
+        if(root != null) {
+            traverse(root.previous());
+            System.out.println(root.getValue());
+            traverse(root.next());
+
+            //step 38 go to main
+
+
+        }
+
+
+    }
 }
+
